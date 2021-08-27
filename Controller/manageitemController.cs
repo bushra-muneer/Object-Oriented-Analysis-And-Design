0c777bb1716e01fa284d71d52adc8cc99f41f109
bushra-muneer <61547124+bushra-muneer@users.noreply.github.com>
2021-08-28 00:04:57 +0500

    Add files via upload

---
+++ Controller/manageitemController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FoodOrdering_ooad_.Entitty;
+using System.Windows.Forms;
+using System.IO;
+using FoodOrdering_ooad_.DbServices;
+
+namespace FoodOrdering_ooad_.Controller
+{
+    class manageitemController
+    {
+        manageitem manage = new manageitem();
+
+
+        DBManue manue = new DBManue();
+        public void InsertItem(string imgurl,ComboBox category,TextBox name,TextBox prie)
+        {
+            byte[] img = null;
+            FileStream fs = new FileStream(imgurl, FileMode.Open, FileAccess.Read);
+            BinaryReader br = new BinaryReader(fs);
+            img = br.ReadBytes((int)fs.Length);
+
+            manage.InserItem(img,category,name,prie);
+        }
+
+
+
+
+
+        public void GetUpdate(int id,ComboBox cat,TextBox name,TextBox price,PictureBox pic)
+        {
+           
+            manue.GetUpdate(id,cat,name,price,pic);
+            
+          
+        
+        }
+
+
+
+        public void Update(string imgurl, ComboBox category, TextBox name, TextBox prie,int id)
+        {
+            byte[] img = null;
+            FileStream fs = new FileStream(imgurl, FileMode.Open, FileAccess.Read);
+            BinaryReader br = new BinaryReader(fs);
+            img = br.ReadBytes((int)fs.Length);
+
+            manue.Update(img,category,name,prie,id);
+
+        }
+
+        public void Update(ComboBox category, TextBox name, TextBox prie, int id)
+        {
+            
+
+            manue.Update(category, name, prie, id);
+
+        }
+
+        public void DeleteItem(int id)
+        {
+            manue.DeleteItem(id);
+        
+        }
+
+        public void ShowItem(ListView lst)
+
+        {
+            lst.Items.Clear();
+            manue.ShowItem(lst);
+        
+        }
+
+
+        public void SearchItem(ComboBox cate,ListView lst)
+        {
+            string category = cate.Text.ToString();
+
+            manue.SearchItem(category, lst);
+        
+        
+        }
+
+
+
+    
+    }
+}
---
+++ Controller/placeorderController.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FoodOrdering_ooad_.Entitty;
+using System.Windows.Forms;
+using FoodOrdering_ooad_.DbServices;
+namespace FoodOrdering_ooad_.Controller
+{
+    class placeorderController
+    {
+        placeorder place = new placeorder();
+
+
+        public void AddStuf(ComboBox itemname,Label price,NumericUpDown quantity)
+        {
+            if (price.Text!="Rs")
+            {
+
+
+                place.ItemName = itemname.Text.ToString();
+                place.Price = int.Parse(price.Text);
+                place.Quantity = int.Parse(quantity.Text);
+
+                place.ItemTotall = place.Price * place.Quantity;
+
+                place.AddItem(place);
+                price.Text = "Rs";
+                quantity.Value = 1;
+            }
+            else
+            {
+
+                MessageBox.Show("select  the manue First");
+             
+            }
+        
+        }
+
+
+
+
+        DBManue manue = new DBManue();
+        public void BurgetList(ComboBox itemname)
+        {
+
+
+            foreach (var item in manue.Burger())
+            {
+               itemname.Items.Add(item);
+            }
+
+        }
+
+
+
+
+        public void TraditionaList(ComboBox itemname)
+        {
+
+
+            foreach (var item in manue.Traditional())
+            {
+                itemname.Items.Add(item);
+            }
+
+        }
+
+
+
+        public void ChickenMealList(ComboBox itemname)
+        {
+
+
+            foreach (var item in manue.ChikenMeal())
+            {
+                itemname.Items.Add(item);
+            }
+
+        }
+
+
+
+        public void PizzaList(ComboBox itemname)
+        {
+
+
+            foreach (var item in manue.Pizza())
+            {
+                itemname.Items.Add(item);
+            }
+
+        }
+
+
+        public void SnacksList(ComboBox itemname)
+        {
+
+
+            foreach (var item in manue.Snacke())
+            {
+                itemname.Items.Add(item);
+            }
+
+        }
+
+
+
+        public void ICecreamList(ComboBox itemname)
+        {
+
+
+            foreach (var item in manue.IceCream())
+            {
+                itemname.Items.Add(item);
+            }
+
+        }
+
+
+        public void   DesssertList(ComboBox itemname)
+        {
+
+
+            foreach (var item in manue.Desserts())
+            {
+                itemname.Items.Add(item);
+            }
+
+        }
+
+
+        public void DrinkList(ComboBox itemname)
+        {
+
+
+            foreach (var item in manue.Drink())
+            {
+                itemname.Items.Add(item);
+            }
+
+        }
+
+
+
+
+
+        public void GetITem(ComboBox itemname,Label pric,PictureBox pic)
+        {
+            if (itemname.Text!="")
+            {
+
+                manue.GetItem(itemname, pric, pic);
+
+            }
+            else 
+            {
+                MessageBox.Show("Select the Item First");
+            
+            }
+        
+        }
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
